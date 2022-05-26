6650781ace5deb275124a13a81fce534aa70407f
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-05-25 21:24:35 -0400

    api url update

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getData = {
-  getWords: () => axios.get(`https://wordler-server.herokuapp.com/`),
+  getWords: () => axios.get(`https://wordler-server.herokuapp.com`),
 };
 
 export default getData;
