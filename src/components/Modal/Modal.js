0cb75f4635ffcec857798a70f46315870ec317f8
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-06-07 11:06:27 -0400

    finished mobile view - the rest of the view are already done

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -13,16 +13,16 @@ export default function Modal({ isCorrect, turn, answer}) {
            <div>
                <h1> You Win </h1>
                <p className="answer"> {answer} </p>
-               <p>You found the solution in {turn} guesses</p>
-               <button onClick={ refreshPage }></button>
+               <p className="modal__guesses">You found the solution in {turn} guesses.</p>
+               <button className="modal__next" onClick={ refreshPage }>Next Word</button>
            </div>
        )}
         {!isCorrect && (
            <div>
                <h1> You Lose </h1>
                <p className="answer"> {answer} </p>
-               <p>Better luck next time</p>
-               <button onClick={ refreshPage }></button>
+               <p>Better luck next time.</p>
+               <button className="modal__next" onClick={ refreshPage }>Next Word</button>
            </div>
        )}
       </div>
